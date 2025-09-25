049727694aa77e29c3069c7fdf2a3cb8ecfb8921
Darkinfect <vectordifenstyeeder@gmail.com>
2025-09-25 17:51:33 +0300

    pr04v2

--- src/RecRelation/pr04.java
+++ src/RecRelation/pr04.java
@@ -7,7 +7,8 @@ import java.util.*;
 
 public class pr04 {
     private static List<String> list = new ArrayList<>();
-    private static List<Integer> iner = new ArrayList<>();
+    private static List<List<Integer>> iner = new ArrayList<>();
+    private static int[][] ss;
     private static void readFile(String name){
         try{
             Path path = Paths.get(System.getProperty("user.dir") +"//input.txt");
@@ -18,21 +19,12 @@ public class pr04 {
     }
     private static void parseMatrixSize(){
         for(int i =1; i < list.size(); i++){
+            List<Integer> arr = new ArrayList<>(2);
             StringTokenizer tokenizer = new StringTokenizer(list.get(i), " ");
-            iner.add(Integer.valueOf(tokenizer.nextToken()));
-            iner.add(Integer.valueOf(tokenizer.nextToken()));
+            arr.add(Integer.parseInt(tokenizer.nextToken()));
+            arr.add(Integer.parseInt(tokenizer.nextToken()));
+            iner.add(arr);
         }
-    }
-    private static int power(int base, int exponent) {
-        int result = 1;
-        while (exponent > 0) {
-            if ((exponent & 1) == 1) {
-                result = (result * base);
-            }
-            base = (base * base);
-            exponent >>= 1;
-        }
-        return result;
     }
     private static void start(){
         readFile("input.txt");
@@ -41,6 +33,8 @@ public class pr04 {
         int[][] arr = new int[countMatrix][countMatrix];
         for(int i = 0; i < arr.length; i++){
             arr[i][i] = 0;
+            if(i +1 == arr.length) break;
+            arr[i][i+1] = iner.get(i).get(0) * iner.get(i).get(1) * iner.get(i+1).get(1);
         }
     }
     public static void main(String[] args) {
