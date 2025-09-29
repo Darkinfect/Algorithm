9b03a79e52b7ed98cffcf93911922b6ddb72144f
Kirill Kuharev <vectordifenstyeeder@gmail.com>
2025-09-29 20:16:45 +0300

    pr07done

--- src/RecRelation/pr07.java
+++ src/RecRelation/pr07.java
@@ -1,5 +1,6 @@
 package RecRelation;
 
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -27,13 +28,38 @@ public class pr07 {
         for(String s: list.get(0).split(" ")){
             integerList.add(Integer.parseInt(s));
         }
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        for(int i = 0; i < n; i++){
-            
+        List<Integer> dp = new ArrayList<>();
+        for(int s: integerList){
+            int left = UpperBound(dp,s);
+            if(left == dp.size()){
+                dp.add(s);
+            }else{
+                dp.set(left,s);
+            }
         }
+        writeinlist(Paths.get(System.getProperty("user.dir") + "\\output.txt"),dp.size());
     }
     public static void main(String[] args) {
         start();
+    }
+    private static int UpperBound(List<Integer> arr, int target){
+        int low =0;
+        int high = arr.size();
+        while(low<high){
+            int mid = low + (high - low)/2;
+            if(arr.get(mid)<target){
+                low = mid + 1;
+            }else{
+                high= mid;
+            }
+        }
+        return low;
+    }
+    private static void writeinlist(Path path,Integer res) {
+        try {
+            Files.write(path, String.valueOf(res).getBytes(StandardCharsets.UTF_8));
+        }catch (Exception exception){
+            exception.printStackTrace();
+        }
     }
 }
