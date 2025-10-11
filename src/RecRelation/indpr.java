24fed611e0ae67ef4be9ab7dccfd9fc02d381010
Kirill Kuharev <vectordifenstyeeder@gmail.com>
2025-10-11 18:01:18 +0300

    indv2

--- src/RecRelation/indpr.java
+++ src/RecRelation/indpr.java
@@ -1,40 +1,37 @@
 package RecRelation;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class indpr {
-    private static List<Integer> integerList = new ArrayList<>();
     private static void start(){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
         int p = 0;
+        int[] massive = new int[n];
         while(n>p){
-            integerList.add(sc.nextInt());
+            massive[p] = sc.nextInt();
             p++;
         }
-        int[][] dp = new int[n][k+1];
+        int[][] dp = new int[n+1][k+1];
+        for (int i = 0; i <= n; i++){
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        }
         dp[0][0] = 0;
-        /*for(int i = 1; i <= n; i++){
-            dp[0][0] =0;
-            dp[i][0] = - Integer.MAX_VALUE;
-            dp[0][i] = - Integer.MAX_VALUE;
-        }*/
 
-        for(int i =1; i < n; i++){
-            for(int t = 1; t<= k; t++){
-                int minval = Integer.MAX_VALUE;
-                for(int j = i-1;j>=t-1; j-- ){
-                    minval = Math.min(minval,integerList.get(j+1));
-                    if(dp[i][t-1] != - Integer.MAX_VALUE){
-                        dp[i][t] = Math.max(dp[i][t], dp[j][t-1] + minval);
+        for(int i =1; i <= n; i++){
+            for(int j = 1; j<= Math.min(i,k); j++){
+                int currMin = Integer.MAX_VALUE;
+                for(int m = i-1; m >= j -1; m-- ){
+                    currMin = Math.min(currMin,massive[m]);
+                    if(dp[m][j-1] != Integer.MIN_VALUE){
+                        dp[i][j] = Math.max(dp[i][j], dp[m][j-1] + currMin);
                     }
                 }
             }
         }
-        System.out.println(dp[n-1][k-1]);
+        System.out.println(dp[n][k]);
     }
     public static void main(String[] args) {
         start();
