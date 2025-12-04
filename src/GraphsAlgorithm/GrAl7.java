52db16f7b46bacaf0caadaaadbcd8a17c4c2e255
Kirill Kuharev <vectordifenstyeeder@gmail.com>
2025-12-04 20:40:54 +0300

    AlgorithmsDone

--- src/GraphsAlgorithm/GrAl7.java
+++ src/GraphsAlgorithm/GrAl7.java
@@ -2,21 +2,93 @@ package GraphsAlgorithm;
 
 import java.io.*;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class GrAl7 {
     public static void main(String[] args) throws FileNotFoundException {
         FastScanner scanner = new FastScanner(new FileInputStream("input.txt"));
         PrintWriter out = new PrintWriter(new FileOutputStream("output.txt"));
-        int n,m;
+
+        int n, m;
         n = scanner.nextInt();
         m = scanner.nextInt();
 
+        int edgeCapacity = 2 * m;
+
+        int[] head = new int[n + 1];
+        int[] to = new int[edgeCapacity];
+        long[] w = new long[edgeCapacity];
+        int[] next = new int[edgeCapacity];
+
+        for (int i = 1; i <= n; i++) head[i] = -1;
+
+        int edgePtr = 0;
+        for (int i = 0; i < m; i++) {
+            int u = scanner.nextInt();
+            int v = scanner.nextInt();
+            long cost = scanner.nextLong();
+
+            to[edgePtr] = v;
+            w[edgePtr] = cost;
+            next[edgePtr] = head[u];
+            head[u] = edgePtr++;
+
+            to[edgePtr] = u;
+            w[edgePtr] = cost;
+            next[edgePtr] = head[v];
+            head[v] = edgePtr++;
+        }
+
+        int start = 1;
+        long[] dist = dijkstra(n, head, to, w, next, start);
+
+        final long INF = Long.MAX_VALUE / 4;
+        if (dist[n] >= INF) {
+            out.println(-1);
+        } else {
+            out.println(dist[n]);
+        }
+
+        out.flush();
+        out.close();
     }
+
+    private static long[] dijkstra(int n, int[] head, int[] to, long[] w, int[] next, int start) {
+        final long INF = Long.MAX_VALUE / 4;
+        long[] dist = new long[n + 1];
+
+        for (int i = 1; i <= n; i++) dist[i] = INF;
+        dist[start] = 0;
+
+        PriorityQueue<Top> pq = new PriorityQueue<>(Comparator.comparingLong(t -> t.distance));
+        pq.add(new Top(start, 0L));
+
+        while (!pq.isEmpty()) {
+            Top cur = pq.poll();
+            int v = cur.num;
+            long d = cur.distance;
+
+            if (d > dist[v]) continue;
+
+            for (int e = head[v]; e != -1; e = next[e]) {
+                int u = to[e];
+                long nd = d + w[e];
+                if (nd < dist[u]) {
+                    dist[u] = nd;
+                    pq.add(new Top(u, nd));
+                }
+            }
+        }
+
+        return dist;
+    }
+
     static final class FastScanner {
         private final InputStream in;
         private final byte[] buffer = new byte[1 << 16];
         private int ptr = 0, len = 0;
-        FastScanner(InputStream is){
+
+        FastScanner(InputStream is) {
             this.in = is;
         }
 
@@ -27,7 +99,7 @@ public class GrAl7 {
                     ptr = 0;
                     if (len <= 0) return -1;
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace(System.err);
             }
             return buffer[ptr++];
@@ -50,7 +122,7 @@ public class GrAl7 {
                     c = read();
                 }
                 return neg ? -x : x;
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace(System.err);
             }
             return -1;
@@ -59,26 +131,15 @@ public class GrAl7 {
         int nextInt() {
             return (int) nextLong();
         }
-
     }
-    static class Top implements Comparator<Top> {
-        private int num;
-        private long distance;
 
-        public Top(int top, int dist){
+    static class Top {
+        int num;
+        long distance;
+
+        public Top(int top, long dist) {
             num = top;
             distance = dist;
         }
-        @Override
-        public int compare(Top top1,Top top2){
-            if(top1.distance>top2.distance){
-                return 1;
-            }else if( top1.distance< top2.distance){
-                return -1;
-            }else{
-                return 0;
-            }
-        }
-
     }
 }
--- src/GraphsAlgorithm/GrAlInd.java
+++ src/GraphsAlgorithm/GrAlInd.java
@@ -1,13 +1,177 @@
 package GraphsAlgorithm;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.*;
 
 public class GrAlInd {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws FileNotFoundException {
+        FastScanner scanner = new FastScanner(new FileInputStream("walk.in"));
+        PrintWriter out = new PrintWriter(new FileOutputStream("walk.out"));
+
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        char[][] lab = new char[m][n];
+        int startRow = -1, startCol = -1;
+        int exitRow = -1, exitCol = -1;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                lab[i][j] = scanner.nextChar();
+                if(lab[i][j] == 'M'){
+                    startRow = i;
+                    startCol = j;
+                } else if(lab[i][j] == 'G'){
+                    exitRow = i;
+                    exitCol = j;
+                }
+            }
+        }
+
+        int result = solve(lab, m, n, startRow, startCol, exitRow, exitCol);
+        out.println(result);
+        out.close();
     }
+
+    private static int solve(char[][] lab, int M, int N,
+                     int startR, int startC, int keyR, int keyC) {
+        if (startR == keyR && startC == keyC) {
+            return 0;
+        }
+
+        int V = M * N;
+        int s = startR * N + startC;
+        int t = keyR * N + keyC;
+
+        MinCostMaxFlow mcmf = new MinCostMaxFlow(V);
+
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+        for (int r = 0; r < M; r++) {
+            for (int c = 0; c < N; c++) {
+                if (lab[r][c] == '*') continue;
+                int u = r * N + c;
+                for (int k = 0; k < 4; k++) {
+                    int nr = r + dr[k];
+                    int nc = c + dc[k];
+                    if (nr < 0 || nr >= M || nc < 0 || nc >= N) continue;
+                    if (lab[nr][nc] == '*') continue;
+                    int v = nr * N + nc;
+                    mcmf.addEdge(u, v);
+                }
+            }
+        }
+
+        long cost = mcmf.minCostMaxFlow(s, t);
+
+        if (cost == Long.MAX_VALUE) {
+            return -1;
+        } else {
+            return (int) cost;
+        }
+    }
+
+    static class MinCostMaxFlow {
+        static class Edge {
+            int to, rev;
+            int cap;
+            int cost;
+            Edge(int to, int rev, int cap, int cost) {
+                this.to = to;
+                this.rev = rev;
+                this.cap = cap;
+                this.cost = cost;
+            }
+        }
+
+        private int n;
+        private List<Edge>[] g;
+
+        MinCostMaxFlow(int n) {
+            this.n = n;
+            g = new ArrayList[n];
+            for (int i = 0; i < n; i++) {
+                g[i] = new ArrayList<>();
+            }
+        }
+
+        private void addEdge(int u, int v) {
+            Edge a = new Edge(v, g[v].size(), 1, 1);
+            Edge b = new Edge(u, g[u].size(), 0, -1);
+            g[u].add(a);
+            g[v].add(b);
+        }
+
+        private long minCostMaxFlow(int s, int t) {
+            final int INF = Integer.MAX_VALUE / 4;
+            int[] dist = new int[n];
+            int[] pot = new int[n];
+            int[] prevV = new int[n];
+            int[] prevE = new int[n];
+
+            int flow = 0;
+            long cost = 0;
+
+            Arrays.fill(pot, 0);
+
+            while (flow < 2) {
+                Arrays.fill(dist, INF);
+                dist[s] = 0;
+                PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+                pq.offer(new int[]{s, 0});
+
+                while (!pq.isEmpty()) {
+                    int[] cur = pq.poll();
+                    int v = cur[0];
+                    int d = cur[1];
+                    if (d != dist[v]) continue;
+                    for (int i = 0; i < g[v].size(); i++) {
+                        Edge e = g[v].get(i);
+                        if (e.cap <= 0) continue;
+                        int nd = d + e.cost + pot[v] - pot[e.to];
+                        if (nd < dist[e.to]) {
+                            dist[e.to] = nd;
+                            prevV[e.to] = v;
+                            prevE[e.to] = i;
+                            pq.offer(new int[]{e.to, nd});
+                        }
+                    }
+                }
+
+                if (dist[t] == INF) {
+                    return Long.MAX_VALUE;
+                }
+
+                for (int v = 0; v < n; v++) {
+                    if (dist[v] < INF) pot[v] += dist[v];
+                }
+
+                int addFlow = 2 - flow;
+                int v = t;
+                while (v != s) {
+                    Edge e = g[prevV[v]].get(prevE[v]);
+                    addFlow = Math.min(addFlow, e.cap);
+                    v = prevV[v];
+                }
+
+                v = t;
+                while (v != s) {
+                    Edge e = g[prevV[v]].get(prevE[v]);
+                    e.cap -= addFlow;
+                    g[v].get(e.rev).cap += addFlow;
+                    v = prevV[v];
+                }
+
+                flow += addFlow;
+                cost += (long) addFlow * pot[t];
+            }
+
+            return cost;
+        }
+    }
+
     static final class FastScanner {
         private final InputStream in;
         private final byte[] buffer = new byte[1 << 16];
@@ -51,6 +215,19 @@ public class GrAlInd {
                 e.printStackTrace(System.err);
             }
             return -1;
+        }
+
+        char nextChar() {
+            try {
+                int c;
+                do {
+                    c = read();
+                } while (c <= 32);
+                return (char) c;
+            } catch (Exception e) {
+                e.printStackTrace(System.err);
+            }
+            return '\0';
         }
 
         int nextInt() {
